a8cfcc8e5a21c6ace8af3213f8f68dc55abd2ae5
isabelweaver95 <izzyweav@gmail.com>
2025-02-08 12:03:02 -0700

    This algorithm works with specific input

--- Sample.py
+++ Sample.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 def get_task_info():
@@ -10,23 +10,34 @@ def get_task_info():
         "task_name": task_name,
         "time": time,
         "priority": priority,
-        "due_date": due_date,
+        "due_date": due_date, # Change this to a date in future. Then you can compare each task's due date
     }
 
+def convert_to_datetime(time_str):
+    return datetime.strptime(time_str, "%H:%M")
+
 def calculate_task_end_time(start_time, time):
-    end_time = start_time + datetime.timedelta(hours=time)
+    end_time = start_time + timedelta(hours=time)
+
     return end_time
 
 def fits_in_time_slot(task, start_time, end_time):
     task_end_time = calculate_task_end_time(start_time, task["time"])
+    return task_end_time <= end_time
+
 
 # This function will be used to make sure the task can get done by t
 def avialable_time_slot():
     task = get_task_info()
-    start_time = input("Enter what time you are free to start the task: ")
+    start_time = input("Enter what time you are free to start the task: ") #This needs to be able to accept a diffent format for time. Like 9:00 AM
+    start_datetime = convert_to_datetime(start_time)
     end_time = input("Enter what time you are free to end the task: ")
+    end_datetime = convert_to_datetime(end_time)
     
-    if(fits_in_time_slot(task, start_time, end_time)):
+    if(fits_in_time_slot(task, start_datetime, end_datetime)):
         print("Task can be done")
     else:
         print("Task can't be done")
+
+
+avialable_time_slot()
