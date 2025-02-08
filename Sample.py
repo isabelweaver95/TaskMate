c9ff73a80b19c057b7f20a40c92ee18c4c7aa7e4
isabelweaver95 <izzyweav@gmail.com>
2025-02-08 12:34:18 -0700

    Added time formating

--- Sample.py
+++ Sample.py
@@ -2,6 +2,8 @@ from datetime import datetime, timedelta
 
 
 def get_task_info():
+    '''
+    This function will get the task info from the user. '''
     task_name = input("Enter the task name: ")
     time = float(input("Enter the task description in hours: "))
     priority = input("Enter the priority (low, medium, high): ")
@@ -14,6 +16,20 @@ def get_task_info():
     }
 
 def convert_to_datetime(time_str):
+    '''This converts the time string to a datetime object. 
+    The time string should be in the format "HH:MM" or "HH:MM AM/PM" '''
+
+    time_str = time_str.strip()
+
+    try:
+        return datetime.strptime(time_str, "%H:%M")
+    except ValueError:
+        try:
+            return datetime.strptime(time_str, "%I:%M %p")
+        except ValueError:
+            raise ValueError("Invalid time format")
+    
+
     return datetime.strptime(time_str, "%H:%M")
 
 def calculate_task_end_time(start_time, time):
@@ -28,11 +44,19 @@ def fits_in_time_slot(task, start_time, end_time):
 
 # This function will be used to make sure the task can get done by t
 def avialable_time_slot():
+    '''
+    This function will get the available time slot for the task, or the time slot that is available.'''
     task = get_task_info()
-    start_time = input("Enter what time you are free to start the task: ") #This needs to be able to accept a diffent format for time. Like 9:00 AM
-    start_datetime = convert_to_datetime(start_time)
+    start_time = input("Enter what time you are free to start the task: ") 
     end_time = input("Enter what time you are free to end the task: ")
-    end_datetime = convert_to_datetime(end_time)
+
+
+    try:
+        start_datetime = convert_to_datetime(start_time)
+        end_datetime = convert_to_datetime(end_time)
+    except ValueError:
+        print("e")
+        return
     
     if(fits_in_time_slot(task, start_datetime, end_datetime)):
         print("Task can be done")
